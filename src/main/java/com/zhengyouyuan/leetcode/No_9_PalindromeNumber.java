dd3b1abffffba29d77d1c8cf429f2b31ab8af89f
zqian724 <zqian724@126.com>
2021-07-15 23:32:51 +0800

    No.9 PalindromeNumber

---
+++ src/main/java/com/zhengyouyuan/leetcode/No_9_PalindromeNumber.java
@@ -0,0 +1,25 @@
+package com.zhengyouyuan.leetcode;
+
+public class No_9_PalindromeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(Solution.isPalindrome(121));
+        System.out.println(Solution.isPalindrome(1221));
+        System.out.println(Solution.isPalindrome(1231));
+        System.out.println(Solution.isPalindrome(12421));
+    }
+
+    static class Solution {
+        public static boolean isPalindrome(int x) {
+            int m = x;
+            if (x < 0)
+                return false;
+            int k = 0;
+            while (x > 0) {
+                k = x % 10 + k * 10;
+                x /= 10;
+            }
+            return m == k;
+        }
+    }
+}
