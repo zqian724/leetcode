d6a349fa92c180275a54f5ce3d951081ef1ebacb
zqian724 <zqian724@126.com>
2021-07-17 22:41:53 +0800

    No.94 BinaryTreeInorderTraversal

---
+++ src/main/java/com/zhengyouyuan/leetcode/No_94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,43 @@
+package com.zhengyouyuan.leetcode;
+
+import java.util.LinkedList;
+
+public class No_94_BinaryTreeInorderTraversal {
+
+    static class Solution {
+        public LinkedList<Integer> inorderTraversal(TreeNode root) {
+            LinkedList<Integer> listInOrder = new LinkedList<>();
+            recursiveTraversal(root, listInOrder);
+            return listInOrder;
+        }
+
+        public void recursiveTraversal(TreeNode node, LinkedList<Integer> list) {
+            if (node == null) {
+                return;
+            }
+            recursiveTraversal(node.left, list);
+            list.add(node.val);
+            recursiveTraversal(node.right, list);
+
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
