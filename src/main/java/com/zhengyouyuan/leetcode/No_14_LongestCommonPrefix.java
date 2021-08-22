99b64f3b1c6f97f67aa4bdf594735a3515749fa7
zqian724 <zqian724@126.com>
2021-08-22 23:18:37 +0800

    No.14 LongestCommonPrefix

---
+++ src/main/java/com/zhengyouyuan/leetcode/No_14_LongestCommonPrefix.java
@@ -0,0 +1,33 @@
+package com.zhengyouyuan.leetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class No_14_LongestCommonPrefix {
+
+    public static void main(String[] args) {
+        String[] strArr1 = new String[]{"flower","flow","flight"};
+        String[] strArr2 = new String[]{"dog","racecar","car"};
+
+        System.out.println(Solution.longestCommonPrefix(strArr1));
+        System.out.println(Solution.longestCommonPrefix(strArr2));
+    }
+
+    static class Solution {
+        public static String longestCommonPrefix(String[] strs) {
+            Arrays.sort(strs, Comparator.comparingInt(String::length));
+            String result = strs[0];
+            for (int i = 1; i < strs.length; i++) {
+                if (result.length() == 0) {
+                    return result;
+                }
+                if (strs[i].startsWith(result)){
+                    continue;
+                }
+                result = result.substring(0, result.length() - 1);
+                i--;
+            }
+            return result;
+        }
+    }
+}
