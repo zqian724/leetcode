299ae2a02ec63135b6238f663b214b9c7e2c611f
zqian724 <zqian724@126.com>
2021-10-13 23:29:46 +0800

    No.387 FirstUniqueCharacterInAString

---
+++ src/main/java/com/zhengyouyuan/leetcode/No_400/No_387_FirstUniqueCharacterInAString.java
@@ -0,0 +1,24 @@
+package com.zhengyouyuan.leetcode.No_400;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class No_387_FirstUniqueCharacterInAString {
+
+    public static void main(String[] args) {
+        System.out.println(Solution.firstUniqChar("leetcode"));
+        System.out.println(Solution.firstUniqChar("loveleetcode"));
+        System.out.println(Solution.firstUniqChar("aabb"));
+    }
+
+    private static class Solution {
+        private static int firstUniqChar(String s) {
+            Map<Character, Integer> map = new LinkedHashMap<>();
+            for (char c : s.toCharArray()) {
+                map.merge(c, 1, Integer::sum);
+            }
+            Character key = map.entrySet().stream().filter(it -> it.getValue() == 1).map(Map.Entry::getKey).findFirst().orElse(null);
+            return key == null ? -1 : s.indexOf(key);
+        }
+    }
+}
